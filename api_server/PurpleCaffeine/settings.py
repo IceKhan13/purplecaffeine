a0993b2df8af1ef2eb8b4b73fa72591271667be8
AnsahMohammad <mohdansah10@gmail.com>
2023-04-27 12:06:19 +0530

    Server Key stored

--- api_server/PurpleCaffeine/settings.py
+++ api_server/PurpleCaffeine/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kb4_a&w(02n4h&5vcuiytouf6iub57$!s&l=uh5z_dlr7=*l67'
+SECRET_KEY = os.getenv('SERV_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
