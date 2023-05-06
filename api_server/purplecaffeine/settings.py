853c53060a68a7083b8a4e48982c3fde541f3013
AnsahMohammad <mohdansah10@gmail.com>
2023-05-06 23:02:07 +0530

    update DEBUG and Hosts to use environment var

--- api_server/purplecaffeine/settings.py
+++ api_server/purplecaffeine/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SERV_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", 0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
