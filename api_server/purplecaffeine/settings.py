9f0fb31d096118acf41235468a52cff3d1ab22db
Mica <mika89700@gmail.com>
2023-05-27 18:57:18 +0200

    black

--- api_server/purplecaffeine/settings.py
+++ api_server/purplecaffeine/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MEDIA_URL = "media/"
