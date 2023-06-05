ee1baf5145b6fc82c5743d3ee20c8680198b804d
AnsahMohammad <mohdansah10@gmail.com>
2023-06-05 21:17:06 +0530

    fixed typo

--- client/tests/test_storage.py
+++ client/tests/test_storage.py
@@ -40,7 +40,7 @@ class TestStorage(TestCase):
         list_trials = self.local_storage.list(query="keep_trial")
         self.assertTrue(isinstance(list_trials, list))
         self.assertTrue(isinstance(list_trials[0], Trial))
-        list_trials = self.local_storage.list(query="keep_trial")
+        list_trials = self.local_storage.list(query="trial999")
         self.assertTrue(isinstance(list_trials, list))
         self.assertEqual(len(list_trials), 0)
 
